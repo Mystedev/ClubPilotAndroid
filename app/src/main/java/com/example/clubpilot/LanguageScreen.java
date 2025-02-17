5b239257b6c9c96a1863740a2aa3fbff283f7016
Dennys <moya-deny14@hotmail.com>
2025-02-17 19:07:13 +0100

    Pantalla inici sessio i seleccio de idioma afegida

--- app/src/main/java/com/example/clubpilot/LanguageScreen.java
+++ app/src/main/java/com/example/clubpilot/LanguageScreen.java
@@ -8,17 +8,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class IniciSessio extends AppCompatActivity {
+public class LanguageScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_inici_sessio2);
+        setContentView(R.layout.language_screen);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
     }
 }
