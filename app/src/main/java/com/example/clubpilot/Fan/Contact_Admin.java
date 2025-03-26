e78cf188a0c12f0e417d7bd1241beaf62b29df33
Dennys <moya-deny14@hotmail.com>
2025-03-26 16:01:16 +0100

    UI actualizada de las pantallas de Fan y fondos personalizados

---
+++ app/src/main/java/com/example/clubpilot/Fan/Contact_Admin.java
@@ -0,0 +1,40 @@
+package com.example.clubpilot.Fan;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.clubpilot.R;
+
+public class Contact_Admin extends AppCompatActivity implements View.OnClickListener{
+    Button btnBack;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_contact_admin);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        btnBack = findViewById(R.id.buttonBack);
+        btnBack.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.buttonBack){
+            Intent intent = new Intent(this, Fan_Configuration.class);
+            startActivity(intent);
+        }
+    }
+}
--- app/src/main/java/com/example/clubpilot/Fan/Fan_Configuration.java
+++ app/src/main/java/com/example/clubpilot/Fan/Fan_Configuration.java
@@ -1,5 +1,7 @@
 package com.example.clubpilot.Fan;
 
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -7,22 +9,27 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.clubpilot.Login;
 import com.example.clubpilot.R;
 
 import java.util.ArrayList;
 
 public class Fan_Configuration extends AppCompatActivity implements View.OnClickListener {
     Button buttonBack;
+    TextView textContactAdmin;
 
     Spinner spinner;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +42,9 @@ public class Fan_Configuration extends AppCompatActivity implements View.OnClick
         });
         buttonBack = findViewById(R.id.buttonBack);
         buttonBack.setOnClickListener(this);
+
+        textContactAdmin = findViewById(R.id.textAccount);
+        textContactAdmin.setOnClickListener(this);
 
         spinner = findViewById(R.id.spinnerIdiomes);
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
@@ -60,9 +70,36 @@ public class Fan_Configuration extends AppCompatActivity implements View.OnClick
     @Override
     public void onClick(View view) {
         if (view.getId() == R.id.buttonBack){
-            Intent intent = new Intent(this, News.class);
-            Toast.makeText(this, "Configuracio guardada", Toast.LENGTH_SHORT).show();
+            showDialogo();
+        }
+        if(view.getId() == R.id.textAccount){
+            Intent intent = new Intent(this, Contact_Admin.class);
             startActivity(intent);
         }
+    }
+
+    public void showDialogo() {
+        // Aquest array conte les opcions que es poden seleccionar
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setPositiveButton("Logout", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                Intent intent = new Intent(Fan_Configuration.this, Login.class);
+                startActivity(intent);
+                dialog.dismiss();
+                Toast.makeText(Fan_Configuration.this, "Has tancat sessió", Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.dismiss();
+            }
+        });
+
+        builder.setTitle("Alert!");
+        builder.setMessage("You're about to logout the session");
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
